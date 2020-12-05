1726dae1eee42dbf322ef05f942035fae8637b21
Nishanth <jnishanthbabu@gmail.com>
2020-12-05 11:04:41 +0530

    template commits

--- constants.py
+++ constants.py
@@ -1,1 +1,23 @@
 # Application wide constants go in this file
+
+NAME = "Name"
+RENT_SITE_ONLY = "Rent - Site Only"
+RENT_HOUSE_ONE = "Rent With One House"
+RENT_HOUSE_TWO = "Rent With Two Houses"
+RENT_HOUSE_THREE = "Rent With Three Houses"
+RENT_HOUSE_FOUR = "Rent With Four Houses"
+RENT_HOTEL = "Rent With Hotel"
+
+# JSON Related Constants
+PROP_NAME = "name"
+PROP_PRICE = "cost"
+PROP_MORTGAGE = "mortgage"
+
+RENT_ONE_HOUSE = "rent_h1"
+RENT_TWO_HOUSE = "rent_h2"
+RENT_THREE_HOUSE = "rent_h3"
+RENT_FOUR_HOUSE = "rent_h4"
+RENT_ONE_HOTEL = "rent_hotel"
+
+HOUSE_COST = "house_cost"
+HOTEL_COST = "hotel_cost"
--- property_creator.py
+++ property_creator.py
@@ -1,22 +1,65 @@
 # Property Creator Application
+import constants
+import json
 
-import PySimpleGUI as sg
+# import PySimpleGUI as sg
+#
+# sg.theme('BluePurple')
+#
+# layout = [[sg.Text('Your typed chars appear here:'), sg.Text(size=(15, 1), key='-OUTPUT-')],
+#           [sg.Input(key='-IN-')],
+#           [sg.Button('Show'), sg.Button('Exit')]]
+#
+# window = sg.Window('Pattern 2B', layout)
+#
+# while True:
+#     event, values = window.read()
+#     print(event, values)
+#     if event == sg.WIN_CLOSED or event == 'Exit':
+#         break
+#     if event == 'Show':
+#         # Update the "output" text element to be the value of "input" element
+#         window['-OUTPUT-'].update(values['-IN-'])
+#
+# window.close()
+colour_list = ["Brown", "Cyan", "Pink", "Orange", "Red", "Yellow", "Green", "Blue"]
+colour_dict = {
+    "Brown": 2,
+    "Cyan": 3,
+    "Pink": 3,
+    "Orange": 3,
+    "Red": 3,
+    "Yellow": 3,
+    "Green": 3,
+    "Blue": 2
+}
+default_properties = dict()
 
-sg.theme('BluePurple')
+for item in colour_list:
+    # Defining a list
+    default_properties[item] = list()
+    # Setting the iter count to 2 if its Brown orBlue property
+    if item == "Brown" or item == "Blue":
+        loop_iter = 2
+    else:
+        loop_iter = 3
 
-layout = [[sg.Text('Your typed chars appear here:'), sg.Text(size=(15, 1), key='-OUTPUT-')],
-          [sg.Input(key='-IN-')],
-          [sg.Button('Show'), sg.Button('Exit')]]
+    for i in range(loop_iter):
+        # Temp dict to push into the list
+        x = dict()
 
-window = sg.Window('Pattern 2B', layout)
+        x[constants.PROP_NAME] = input(f"Enter {i} {item} property name")
+        x[constants.PROP_PRICE] = int(input(f"Enter {i} {item} property cost"))
+        x[constants.PROP_MORTGAGE] = int(input(f"Enter {i} {item} property mortgage"))
+        x[constants.RENT_ONE_HOUSE] = int(input(f"Enter {i} {item} rent with 1 house"))
+        x[constants.RENT_TWO_HOUSE] = int(input(f"Enter {i} {item} rent with 2 house"))
+        x[constants.RENT_THREE_HOUSE] = int(input(f"Enter {i} {item} rent with 3 house"))
+        x[constants.RENT_FOUR_HOUSE] = int(input(f"Enter {i} {item} rent with 4 house"))
+        x[constants.RENT_ONE_HOTEL] = int(input(f"Enter {i} {item} rent with 1 hotel"))
+        x[constants.HOUSE_COST] = int(input(f"Enter {i} {item} Cost of  house"))
+        x[constants.HOTEL_COST] = int(input(f"Enter {i} {item} cost of hotel"))
 
-while True:
-    event, values = window.read()
-    print(event, values)
-    if event == sg.WIN_CLOSED or event == 'Exit':
-        break
-    if event == 'Show':
-        # Update the "output" text element to be the value of "input" element
-        window['-OUTPUT-'].update(values['-IN-'])
+        default_properties[item].append(x)
 
-window.close()
+with open("defaults.json", 'w')as outputfile:
+    json.dump(default_properties, outputfile, indent=4)
